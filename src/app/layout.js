95ed7a09a18518079bf28edfb8fa61c86822ad52
pollii84 <polimoga@gmail.com>
2026-08-05 07:54:07 +0300

    add Google Tag (gtag.js) to root layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script';
 import Providers from '@/components/Providers';
 import '@/styles/globals.css';
 
@@ -10,9 +11,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-R2BJ2TQ69C"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-R2BJ2TQ69C');
+          `}
+        </Script>
+      </head>
       <body>
         <Providers>{children}</Providers>
       </body>
     </html>
   );
 }
+
